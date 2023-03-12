879d76b8fe58ae578f7272b1b81fbe468b0899a2
Karpuhin Vladimir <vkarpuhin@bk.ru>
2023-03-12 09:55:07 +0400

    Выполнена задача №66

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Необходимо найти сумму натуральных элементов в промежутке от M до N.
+
+int Sum(int a, int b)
+{
+    if (a == 0) 
+        return (b * (b + 1)) / 2;            // Если a равно нулю
+    else if (b == 0) 
+        return (a * (a + 1)) / 2;            // Если b равно нулю
+    else if (a == b) 
+        return a;                            // Если a=b
+    else if (a < b) 
+        return b + Sum(a, b - 1);            // Если a<b
+    else 
+        return b + Sum(a, b + 1);            // Если a>b
+}
+
+Console.Clear();
+Console.Write("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: {Sum(M, N)}");
